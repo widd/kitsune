e4e49826e44b784b68f05c820398afa5f995f3b6
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-10 15:01:35 -0800

    Requires Composer's auto-loader

--- RunWorld.php
+++ RunWorld.php
@@ -6,6 +6,8 @@ date_default_timezone_set("America/Los_Angeles");
 
 error_reporting(E_ALL ^ E_STRICT);
 
+require_once 'vendor/autoload.php';
+
 spl_autoload_register(function ($path) {
 	$realPath = str_replace("\\", "/", $path) . ".php";
 	$includeSuccess = include_once $realPath;
@@ -16,7 +18,9 @@ spl_autoload_register(function ($path) {
 });
 
 $cp = new World();
+
 $cp->listen(0, 9875);
+
 while(true) {
 	$cp->acceptClients();
 }
